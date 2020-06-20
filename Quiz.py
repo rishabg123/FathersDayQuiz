32872cf4e08b7178db23dd1956dad51e9a22d978
Rishab Gupta <rishabg2004@gmail.com>
2020-06-20 10:55:39 -0700

    Update for Ayush to takeover

--- Quiz.py
+++ Quiz.py
@@ -1,10 +1,13 @@
 count = 0
-
-print("Question 1: ")
+print("Welcome to the Father's Day Quiz designed by Rishab and Ayush")
+print("--------------------------------------------------------------")
+print("Question 1: What year was the iPhone released")
 answer1 = str(input())
 
-if answer1 == "":
+if answer1 == "2004":
     print("Correct")
     count += 1
 else:
-    
+    print("That is wrong")
+    print("The answer was 2004")
+
